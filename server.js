6203dae9ec2cad84c6fa7aab85cafb4c8548d760
Ankit Patil <ankitpatil9307@gmail.com>
2024-12-26 12:12:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let todos = [
+  { id: 1, task: 'Learn Node.js' },
+  { id: 2, task: 'Build a To-Do app' }
+];
+
+// Get all todos
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+// Add a new todo
+app.post('/todos', (req, res) => {
+  const newTodo = { id: todos.length + 1, task: req.body.task };
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+// Delete a todo
+app.delete('/todos/:id', (req, res) => {
+  todos = todos.filter(todo => todo.id !== parseInt(req.params.id));
+  res.json({ message: 'Todo deleted' });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
